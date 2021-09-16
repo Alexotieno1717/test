905b196146edee6f211c07ea76ef7d65b160e844
Alexotieno1717 <alexotieno900@gmail.com>
2021-09-15 22:47:27 -0700

    Test code

---
+++ main.js
@@ -0,0 +1,52 @@
+// QUESTION
+
+/* Picture yourself as a mechanic in Nakuru, you are tired of living in Nakuru and want to make a switch to
+Nairobi. You think it might be a better career move as Nairobi is a big metropolitan city with lots of
+personal cars which is your area of specialty. Your cousin tells you that there are 500 personal car
+mechanics in Nairobi, calculate whether or not it’s worth it for you to move and set up your business in
+Nairobi. There’s No right or Wrong answer, we are interested in the logic. */
+
+
+// lOGIC 
+
+// Personally, first this guy already doesn't want to live in Nakuru. That's a mind blockage and might also affect his productivity.
+// The reason why he doesn't want to live their? We don't know
+// So, he's thinking of moving but hasn't found an ideal location
+// "There are 500 personal mechanics in Nairobi" You need to validate this first
+// Then we ask ourselves, what does it take to set up a business in Nairobi, given that he had already set up the business hear in Nakuru
+// Should he sell the business and go start afresh?
+// Should he transport the tools to Nairobi? And how much would it cost?
+// Which is the best location for setting up the shop? Will the operation cost be higher or lower compares to Nakuru?
+// Does he have a strategy to tap on the customers? And why is there only 500 personal mechanics? Is it that the job is scarce, less profitable, or a market niche?
+// Another thing, why doesn't he want to live in Nakuru? Is it something he has power over it and can change? And if he restrategize his business model their in Nakuru, does he think it will yield more profit and also become feasible as compared to setting up a new shop?
+// So you need to just pose Nairobi with Nakuru based on the questions above and come up with the values.
+// But personally, I don't think moving to Nairobi is feasible. He can redesign his business model to marry with the local market
+
+
+
+let numMechanicInNairobi = 500
+let numMechanicInNakuru = x
+
+if(numMechanicInNairobi <= numMechanicInNakuru){
+    return alert('Their is no neeed of moving from Nakuru to set up a businesss in a Nairobi')
+}else if(numMechanicInNairobi === numMechanicInNakuru){
+    return alert('You should consider the a mount of starting a fresh businesss in Nairobi')
+}else{
+    return alert('There is no need of moving from Nakuru to Nairobi');
+};
+
+
+let newBusinessCostNairobi = 'notStarted';
+let businessCostNakuru   = 'started';
+
+if(newBusinessCostNairobi === started){
+    return alert('consider the profits you get after the deductibles.')
+}else if(businessCostNakuru === started){
+    return alert("I don't think moving to Nairobi is feasible. He can redesign his business model to marry with the local market")
+}else{
+    return alert('starting a new business will cost higher amount.')
+}
+console.log(newBusinessCostNairobi)
+// My Conclusion
+//First before moving you have to consider the consider the cost of setting up the business in a new place and customer acquisition rate.You need some good savings for this You also need to consider the profits you get after the deductibles . In the end working and establish yourself would be wiser because although the clients might be fewer but the profits you get from it would be higher than in Nairobi.
+
